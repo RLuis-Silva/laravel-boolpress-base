06d1eb617da2ba2770cc4eba9e53e2c8aada51aa
Luis <s.ros.lui@gmail.com>
2021-01-26 07:22:42 +0100

    edit main hompage (home.blade.php)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts/main')
 @section('main-content')
-    main qui
+    <h1>Hompage Blog</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias placeat fuga adipisci cumque distinctio vel, quia velit nam inventore sunt omnis natus earum dolor dolore recusandae nostrum nisi excepturi exercitationem?</p>
 @endsection
