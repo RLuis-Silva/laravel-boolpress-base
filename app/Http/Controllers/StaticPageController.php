ebe2ce8e1f81f051c173b49fe46c906ea292cedd
Luis <s.ros.lui@gmail.com>
2021-01-26 10:53:58 +0100

    add tabella 'post'- migration

--- app/Http/Controllers/StaticPageController.php
+++ app/Http/Controllers/StaticPageController.php
@@ -6,10 +6,16 @@ use Illuminate\Http\Request;
 
 class StaticPageController extends Controller
 {
+    /**
+     * pagine statiche
+     */
+
+    // HOME
     public function home(){
         return view('home');
     }
 
+    // ABOUT
     public function about(){
         return view('about');
     }
