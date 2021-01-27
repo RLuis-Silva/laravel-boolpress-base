548b051f9e3dd9c8a42462bce81fd48f540651a0
Luis <s.ros.lui@gmail.com>
2021-01-27 07:38:27 +0100

    add markup 'new post' (add form)

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,5 +2,33 @@
 @section('main-content')
     <div class="container mb-5">
         <h1>crea un nuovo post!</h1>
+
+        <form action="{{ route('posts.store') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            @method('POST')
+
+            {{-- FORM-titolo --}}
+            <div class="form-group">
+                <label for="title">Title</label>
+                <input class="form-control" type="text" name="title" id="title" value="{{ old('title') }}">
+            </div>
+
+            {{-- FORM-contenuto --}}
+            <div class="form-group">
+                <label for="body">Description</label>
+                <textarea class="form-control" type="text" name="body" id="body"> {{ old('body') }}</textarea>
+            </div>
+
+            <div class="form-group">
+                <label for="path_img">Post image</label>
+                <input type="file" name="path_img" id="path_img" accept="image/*">
+            </div>
+
+            {{-- btn SUBMIT --}}
+            <div class="form-group">
+               <input type="submit" class="btn btn-primary" value="Create post">
+            </div>
+
+        </form>
     </div>
 @endsection
