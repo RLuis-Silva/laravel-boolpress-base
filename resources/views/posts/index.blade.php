4d26b448c72b313d7188c80c17659c0724ab0314
Luis <s.ros.lui@gmail.com>
2021-01-27 06:44:05 +0100

    add testo di feedback in blog

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,5 +3,18 @@
     <div class="container mb-5">
         <h1>Blog Archive</h1>
         
+        {{-- se dato presente in DB --}}
+        @forelse ($posts as $post)
+            <article class="mb-5">
+                <h2>{{ $post->title }}</h2>
+                <h5>{{ $post->created_at -> format('d/m/Y') }}</h5>
+                <p>{{ $post->body }}</p>
+                <a href="#"> Read more</a>
+            </article>
+        {{-- se nessun dato presente in DB --}}
+        @empty
+            <p>No post found. Go and <a href="#">create a new one</a> </p>
+        @endforelse
+
     </div>
 @endsection
